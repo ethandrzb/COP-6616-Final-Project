4a25243df8df038e0dd939c855e6ea47f48e5846
Ethan Thomas <ethandrzb@gmail.com>
2025-12-02 15:48:49 -0500

    Enable access to shared memory region

--- COP-6616-Final-Project-Scenario-2/CM7/Core/Src/main.c
+++ COP-6616-Final-Project-Scenario-2/CM7/Core/Src/main.c
@@ -388,7 +388,7 @@ void MPU_Config(void)
   MPU_InitStruct.Size = MPU_REGION_SIZE_64KB;
   MPU_InitStruct.SubRegionDisable = 0x0;
   MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-  MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
   MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
   MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
   MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
