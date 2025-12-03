4e0494258d6661654661adef910bf17515f6679f
Ethan Thomas <ethandrzb@gmail.com>
2025-12-03 01:30:52 -0500

    Make room in SRAM4 for DMA handle pointers

--- COP-6616-Final-Project-Scenario-2/Common/Inc/common.h
+++ COP-6616-Final-Project-Scenario-2/Common/Inc/common.h
@@ -14,13 +14,13 @@
 #define BUFF_CM4_TO_CM7_ADDR                MEM_ALIGN(SHD_RAM_START_ADDR)
 #define BUFF_CM4_TO_CM7_LEN                 MEM_ALIGN(sizeof(ringbuf_t))
 #define BUFFDATA_CM4_TO_CM7_ADDR            MEM_ALIGN(BUFF_CM4_TO_CM7_ADDR + BUFF_CM4_TO_CM7_LEN)
-#define BUFFDATA_CM4_TO_CM7_LEN             MEM_ALIGN(0x00007FF0)	// Default is  0x00000400
+#define BUFFDATA_CM4_TO_CM7_LEN             MEM_ALIGN(0x00007F00)	// Default is  0x00000400
 
 /* Buffer from CM7 to CM4 */
 #define BUFF_CM7_TO_CM4_ADDR                MEM_ALIGN(BUFFDATA_CM4_TO_CM7_ADDR + BUFFDATA_CM4_TO_CM7_LEN)
 #define BUFF_CM7_TO_CM4_LEN                 MEM_ALIGN(sizeof(ringbuf_t))
 #define BUFFDATA_CM7_TO_CM4_ADDR            MEM_ALIGN(BUFF_CM7_TO_CM4_ADDR + BUFF_CM7_TO_CM4_LEN)
-#define BUFFDATA_CM7_TO_CM4_LEN             MEM_ALIGN(0x00007FF0)	// Default is  0x00000400
+#define BUFFDATA_CM7_TO_CM4_LEN             MEM_ALIGN(0x00007F00)	// Default is  0x00000400
 
 #define TEST_BUFFER_SIZE 1024
 
